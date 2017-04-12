4d828a37c74c94145a9d18b191db42a076db7f40
rubenVillca <ruben_wolf_9@hotmail.com>
2017-04-11 23:04:46 -0400

    View createAccount

--- app/src/main/java/com/umss/sistemas/tesis/hotel/LoginActivity.java
+++ app/src/main/java/com/umss/sistemas/tesis/hotel/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.umss.sistemas.tesis.hotel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.umss.sistemas.tesis.hotel.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void goCreateAccount(View view){
+        Intent intent =new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/umss/sistemas/tesis/hotel/view/CreateAccountActivity.java
@@ -0,0 +1,24 @@
+package com.umss.sistemas.tesis.hotel.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.umss.sistemas.tesis.hotel.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        showToolBar(getResources().getString(R.string.toolbar_tittle_create_account),true);
+    }
+
+    public void showToolBar(String tittle,boolean upButton){
+        Toolbar toolbar =(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+}

ac1f8c384b8e1ffc170cde0295a09a1ec4674918
rubenVillca <ruben_wolf_9@hotmail.com>
2017-04-17 01:54:45 -0400

    Adapter services

--- app/src/main/java/com/umss/sistemas/tesis/hotel/activity/ServicesActivity.java
+++ app/src/main/java/com/umss/sistemas/tesis/hotel/activity/ServicesActivity.java
@@ -1,10 +1,15 @@
 package com.umss.sistemas.tesis.hotel.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.umss.sistemas.tesis.hotel.R;
+import com.umss.sistemas.tesis.hotel.adapter.HomeAdapterRecycler;
+import com.umss.sistemas.tesis.hotel.model.Picture;
 import com.umss.sistemas.tesis.hotel.util.Activities;
+
+import java.util.ArrayList;
 
 public class ServicesActivity extends Activities {
 
@@ -13,5 +18,30 @@ public class ServicesActivity extends Activities {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
         super.showToolBar(getResources().getString(R.string.toolbar_tittle_services),true);
+        setRecyclerView();
+    }
+
+    private void setRecyclerView() {
+        RecyclerView pictureRecycler=(RecyclerView)findViewById(R.id.homeRecyclerView);
+
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        pictureRecycler.setLayoutManager(linearLayoutManager);
+
+        HomeAdapterRecycler homeAdapter=new HomeAdapterRecycler(buidPicture(),R.layout.cardview_service,this);
+        pictureRecycler.setAdapter(homeAdapter);
+    }
+
+    public ArrayList<Picture> buidPicture(){
+        ArrayList<Picture> pictures= new ArrayList<>();
+
+        pictures.add(new Picture("https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Sasso_lungo_da_passo_pordoi.jpg/270px-Sasso_lungo_da_passo_pordoi.jpg","Mario","4 dias","3  me gusta"));
+        pictures.add(new Picture("https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Sasso_lungo_da_passo_pordoi.jpg/270px-Sasso_lungo_da_passo_pordoi.jpg","Marioas","7 dias","4"));
+        pictures.add(new Picture("https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Sasso_lungo_da_passo_pordoi.jpg/270px-Sasso_lungo_da_passo_pordoi.jpg","Marioas","7 dias","5 me gusta"));
+        pictures.add(new Picture("https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Sasso_lungo_da_passo_pordoi.jpg/270px-Sasso_lungo_da_passo_pordoi.jpg","Marioas","7 dias","6  me gusta"));
+        pictures.add(new Picture("https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Sasso_lungo_da_passo_pordoi.jpg/270px-Sasso_lungo_da_passo_pordoi.jpg","Marioas","7 dias","7  me gusta"));
+
+        return pictures;
     }
 }
--- app/src/main/java/com/umss/sistemas/tesis/hotel/fragments/ServicesFragment.java
+++ app/src/main/java/com/umss/sistemas/tesis/hotel/fragments/ServicesFragment.java
@@ -35,33 +35,9 @@ public class ServicesFragment extends Fragments {
         super.showToolBar(getResources().getString(R.string.tab_service),false,view);
         super.showFloatingButtonMessage(view);
 
-        view=setRecyclerView(view);
 
         return view;
     }
 
-    private View setRecyclerView(View view) {
-        RecyclerView pictureRecycler=(RecyclerView)view.findViewById(R.id.homeRecyclerView);
 
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext());
-        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-
-        pictureRecycler.setLayoutManager(linearLayoutManager);
-
-        HomeAdapterRecycler homeAdapter=new HomeAdapterRecycler(buidPicture(),R.layout.cardview_service,getActivity());
-        pictureRecycler.setAdapter(homeAdapter);
-        return view;
-    }
-
-    public ArrayList<Picture> buidPicture(){
-        ArrayList<Picture> pictures= new ArrayList<>();
-
-        pictures.add(new Picture("https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Sasso_lungo_da_passo_pordoi.jpg/270px-Sasso_lungo_da_passo_pordoi.jpg","Mario","4 dias","3  me gusta"));
-        pictures.add(new Picture("https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Sasso_lungo_da_passo_pordoi.jpg/270px-Sasso_lungo_da_passo_pordoi.jpg","Marioas","7 dias","4"));
-        pictures.add(new Picture("https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Sasso_lungo_da_passo_pordoi.jpg/270px-Sasso_lungo_da_passo_pordoi.jpg","Marioas","7 dias","5 me gusta"));
-        pictures.add(new Picture("https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Sasso_lungo_da_passo_pordoi.jpg/270px-Sasso_lungo_da_passo_pordoi.jpg","Marioas","7 dias","6  me gusta"));
-        pictures.add(new Picture("https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Sasso_lungo_da_passo_pordoi.jpg/270px-Sasso_lungo_da_passo_pordoi.jpg","Marioas","7 dias","7  me gusta"));
-
-        return pictures;
-    }
 }
